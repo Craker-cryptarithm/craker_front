f6ee31bddff19129f08055d1db512d5a72b06636
wanwan0622 <minnie20000622@yahoo.co.jp>
2020-09-11 16:58:27 +0900

    エラーメッセージの表示

--- front.py
+++ front.py
@@ -1,7 +1,15 @@
 # - * - coding: utf8 - * -
 
+#tkinterの準備
 import tkinter as tk
 from tkinter import messagebox
+
+#バックを利用するのに使うやつ
+import sys
+sys.path.append('..')
+from craker_back import back
+
+
 
 ##フレームの作成
 root = tk.Tk()
@@ -13,8 +21,8 @@ root.title("craker")
 root.geometry("700x1000")
 
 ##ラベル作成
-label_question = tk.Label(root, text = "虫食い算")
-label_question.place(relx = 0.45, rely = 0.01)
+label_title = tk.Label(root, text = "虫食い算")
+label_title.place(relx = 0.45, rely = 0.01)
 
 label_diff = tk.Label(root, text = "難易度(0~9)")
 label_diff.place(relx = 0.35, rely = 0.05)
@@ -28,6 +36,23 @@ txt_diff.place(relx = 0.55, rely = 0.05)
 
 txt_digits = tk.Entry(width = 3)
 txt_digits.place(relx = 0.55, rely = 0.1)
+
+##ボタンが押された時に実行される関数
+def clicked(self):
+    input_diff = int(txt_diff.get())
+    input_digits = int(txt_digits.get())
+    if input_diff < 0 or input_diff > 9:
+        res_diff_error = messagebox.showwarning("エラー", "難易度は0~9の数字で答えてください")
+        print("showwarning", res_diff_error)
+    if input_digits <= 0 or input_digits >= 6:
+        res_digits_error = messagebox.showwarning("エラー", "桁数は1~6の数字で答えてください")
+        print("showwarning", res_digits_error)
+    pass
+
+##ボタン作成
+button = tk.Button(root, text = "問題を作成！")
+button.bind("<Button-1>", clicked)
+button.place(relx = 0.6, rely = 0.15)
 
 """
 var_2 = tk.StringVar()
@@ -46,13 +71,7 @@ def clicked(self):
     return var_2
 label_ans = tk.Label(root, textvariable = var_2, justify = 'left', font = "VLゴシック")
 label_ans.place(x = 10, y = 60)
-
-##ボタンの作成
-#var_3 = tk.StringVar()
-#var_3.set("PUSH")
-button = tk.Button(root, text = "PUSH", fg = "blue", bg = "red")
-button.bind("<Button-1>",clicked)
-button.place(x = 150, y = 30)
 """
+
 ##イベントループ
 root.mainloop()
