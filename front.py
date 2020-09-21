875a11eec2bcca235795c9ef1bbae9d1db2858db
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-09-21 15:29:54 +0900

    位置調整 :)

--- front.py
+++ front.py
@@ -13,17 +13,17 @@ from tkinter import messagebox
 #バックを利用する
 import sys
 sys.path.append('..')
-#from craker_back import back
+from craker_back import back
 
 
 ##ウィンドウの作成
 root = tk.Tk()
 root.resizable(width = False, height = False)
 root.title("craker")
-canvas = tk.Canvas(root, width = 450, heigh = 620)
+canvas = tk.Canvas(root, width = 1280, heigh = 720)
 canvas.pack()
 img = tk.PhotoImage(file = "background.gif")
-canvas.create_image(225, 310, image = img)
+canvas.create_image(640, 360, image = img)
 
 ##アイコン設定
 root.iconbitmap("icon.ico")
@@ -31,8 +31,8 @@ root.iconbitmap("icon.ico")
 ##ラベル表示
 txt_menu = tk.StringVar()
 txt_menu.set("モードを\n選んでね↓")
-label_menu = tk.Label(root, textvariable = txt_menu, bg = "#FFFFFF")
-label_menu.place(relx = 0.76, rely = 0.02)
+label_menu = tk.Label(root, textvariable = txt_menu, bg = "#FFFFFF", font = ("", 20))
+label_menu.place(relx = 0.79, rely = 0.02)
 
 whitchCalculatuion = -1
 
@@ -42,7 +42,7 @@ def createNewWindow():
     label_menu.destroy()
     global whitchCalculatuion
     newWindow = tk.Toplevel(root)
-    newWindow.geometry("150x200")
+    newWindow.geometry("150x130")
     newWindow.resizable(width = False, height = False)
     newWindow.title("モード選択")
 
@@ -69,42 +69,42 @@ def createNewWindow():
         txt_mode.set("わり算モード")
         newWindow.destroy()
     
-    button_addition = tk.Button(newWindow, text = "たし算", bg = "#FFFFFF", fg = "#000000", command = clicked_addition)
+    button_addition = tk.Button(newWindow, text = "たし算", bg = "#FFFFFF", fg = "#000000", command = clicked_addition, font = ("", 12))
     button_addition.pack()
 
-    button_subtraction = tk.Button(newWindow, text = "ひき算", bg = "#FFFFFF", fg = "#000000", command = clicked_subtraction)
+    button_subtraction = tk.Button(newWindow, text = "ひき算", bg = "#FFFFFF", fg = "#000000", command = clicked_subtraction, font = ("", 12))
     button_subtraction.pack()
 
-    button_multiplication = tk.Button(newWindow, text = "かけ算", bg = "#FFFFFF", fg = "#000000", command = clicked_multiplication)
+    button_multiplication = tk.Button(newWindow, text = "かけ算", bg = "#FFFFFF", fg = "#000000", command = clicked_multiplication, font = ("", 12))
     button_multiplication.pack()
 
-    button_division = tk.Button(newWindow, text = "わり算", bg = "#FFFFFF", fg = "#000000", command = clicked_division)
+    button_division = tk.Button(newWindow, text = "わり算", bg = "#FFFFFF", fg = "#000000", command = clicked_division, font = ("", 12))
     button_division.pack()
 
-    label_mode = tk.Label(root, textvariable = txt_mode, bg = "#FFFFFF")
-    label_mode.place(relx = 0.76, rely = 0.03)
+    label_mode = tk.Label(root, textvariable = txt_mode, bg = "#FFFFFF", font = ("", 20))
+    label_mode.place(relx = 0.79, rely = 0.05)
 
 
-button_NewWindow = tk.Button(root, text = "モードを選ぶ", command = createNewWindow)
-button_NewWindow.place(relx = 0.73, rely = 0.09)
+button_NewWindow = tk.Button(root, text = "モードを選ぶ", command = createNewWindow, font = ("", 12), bg = "#FFFFFF")
+button_NewWindow.place(relx = 0.8, rely = 0.1)
 
 
 ##ラベル作成
-label_digits = tk.Label(root, text = "桁数", bg = "#FFFFFF")
-label_digits.place(relx = 0.92, rely = 0.134)
+label_digits = tk.Label(root, text = "桁数", bg = "#FFFFFF", font = ("", 13))
+label_digits.place(relx = 0.018, rely = 0.23)
 
 ##テキストボックスの作成
-txt_digits = tk.Entry(width = 2, bg = "#cff1ef")
-txt_digits.place(relx = 0.94, rely = 0.184)
+txt_digits = tk.Entry(width = 2, bg = "#cff1ef", font = ("", 18))
+txt_digits.place(relx = 0.02, rely = 0.3)
 
 ##スケールの作成
-label_difficult = tk.Label(root, text = "難", bg = "#FFFFFF")
-label_easy = tk.Label(root, text = "易", bg = "#FFFFFF")
-label_difficult.place(relx = 0.87, rely = 0.134)
-label_easy.place(relx = 0.68, rely = 0.134)
+label_difficult = tk.Label(root, text = "難", bg = "#FFFFFF", font = ("", 13))
+label_easy = tk.Label(root, text = "易", bg = "#FFFFFF", font = ("", 13))
+label_difficult.place(relx = 0.19, rely = 0.23)
+label_easy.place(relx = 0.05, rely = 0.23)
 var_input_diff = tk.IntVar(master = root, value = 0)
-scale_diff = tk.Scale(root, orient = 'h', from_ = 0, to = 9, variable = var_input_diff, showvalue = False, resolution = 1, length = 100)
-scale_diff.place(relx = 0.68, rely = 0.186)
+scale_diff = tk.Scale(root, orient = 'h', from_ = 0, to = 9, variable = var_input_diff, showvalue = False, resolution = 1, length = 200)
+scale_diff.place(relx = 0.05, rely = 0.305)
 
 ##グローバル変数の宣言
 problem = None
@@ -127,6 +127,10 @@ def clicked_problem(self):
     input_digits = txt_digits.get()
 
     ###エラーメッセージ
+    if whitchCalculatuion == -1:
+        res_mode_error + messagebox.showwarning("エラー", "モードを選んでください")
+        print("showwarning", res_mode_error)
+        return 1
     if input_digits == "":  #入力されていないとき
         res_null_error = messagebox.showwarning("エラー", "桁数を入力してください")
         print("showwarning", res_null_error)
@@ -152,11 +156,9 @@ def clicked_problem(self):
     if whitchCalculatuion == 2:
         button_problem.config(state = 'disable')
         problem, ans2, diff = back.problem_maker_multiplication(input_diff, input_digits)
-    """
     if whitchCalculatuion == 3:
         button_problem.config(state = 'disable')
         problem, ans3, diff = back.problem_maker_division(input_diff, input_digits)
-"""
 
     if problem == -1:   #タイムアウトが発生したとき
         res_error = messagebox.showwarning("エラー", "解が見つかりませんでした\nもう一度試してください")
@@ -168,15 +170,15 @@ def clicked_problem(self):
     var_ans.set("")
 
 ##clicked_problem実行時のラベル
-label_problem = tk.Label(root, textvariable = var_problem, font = ("Courier", 10), bg = "#D54A43", fg = "#FFFFFF")
-label_problem.place(relx = 0.46, rely = 0.35, anchor = "center")
-label_real_diff = tk.Label(root, textvariable = var_diff, font = ("Courier", 10), bg = "#5F8247", fg = "#FFFFFF")
-label_real_diff.place(relx = 0.72, rely = 0.5)
+label_problem = tk.Label(root, textvariable = var_problem, font = ("Courier", 12), bg = "#D54A43", fg = "#FFFFFF")
+label_problem.place(relx = 0.32, rely = 0.62, anchor = "center")
+label_real_diff = tk.Label(root, textvariable = var_diff, font = ("Courier", 14), bg = "#FFFFFF", fg = "#000000")
+label_real_diff.place(relx = 0.03, rely = 0.8)
 
 ##問題作成ボタン作成
-button_problem = tk.Button(root, text = "問題を\n作成！", bg = "#D54A43", fg = "#FFFFFF", height = 3, width = 7)
+button_problem = tk.Button(root, text = "問題を\n作成！", bg = "#D54A43", fg = "#FFFFFF", height = 3, width = 7, font = ("", 15))
 button_problem.bind("<Button-1>", clicked_problem)
-button_problem.place(relx = 0.06, rely = 0.185)
+button_problem.place(relx = 0.475, rely = 0.295)
 
 
 ##答えを見るボタンが押された時に実行される関数
@@ -187,19 +189,17 @@ def clicked_ans(self):
         var_ans.set(ans1)
     if whitchCalculatuion == 2:
         var_ans.set(ans2)
-    """
     if whitchCalculatuion == 3:
         var_ans.set(ans3)
-    """
 
 #clicked_ans実行時のラベル
-label_ans = tk.Label(root, textvariable = var_ans, font = ("Courier", 10), bg = "#D54A43", fg = "#FFFFFF")
-label_ans.place(relx = 0.46, rely = 0.73, anchor = "center")
+label_ans = tk.Label(root, textvariable = var_ans, font = ("Courier", 12  ), bg = "#D54A43", fg = "#FFFFFF")
+label_ans.place(relx = 0.75, rely = 0.62, anchor = "center")
 
 ##答えを見るボタンを作成
-button_ans = tk.Button(root, text = "答えを\n見る！", bg = "#D54A43", fg = "#FFFFFF", height = 3, width = 7)
+button_ans = tk.Button(root, text = "答えを\n見る！", bg = "#D54A43", fg = "#FFFFFF", height = 3, width = 7, font = ("", 15))
 button_ans.bind("<Button-1>", clicked_ans)
-button_ans.place(relx = 0.06, rely = 0.55)
+button_ans.place(relx = 0.898, rely = 0.3)
 
 
 ##使い方ボタンがクリックされた時に実行される関数
@@ -208,9 +208,9 @@ def clicked_readme(self):
     print("showinfo", res_readme)
 
 ##使い方ボタン
-button_readme = tk.Button(root, text = "使い方を見る", bg = "#FFFFFF", height = 2, width = 10)
+button_readme = tk.Button(root, text = "使い方を見る", bg = "#FFFFFF", font = ("", 19))
 button_readme.bind("<Button-1>", clicked_readme)
-button_readme.place(relx = 0.037, rely = 0.02)
+button_readme.place(relx = 0.096, rely = 0.065)
 
 
 ##イベントループ
